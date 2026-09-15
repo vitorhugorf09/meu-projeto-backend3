f5581b8372c328f26651234e967d422ffd6a7f66
I1HSC <vitorhugorf09@gmail.com>
2026-09-15 10:46:32 -0300

    Projeto backend3

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,67 @@
 // Importa a biblioteca Express e também o tipo Express
 // O Express será utilizado para criar o servidor web
 import express from "express";
-import type { Express } from "express";
+import type { Express, Request, Response } from "express";
+//importa a classe Player do arquivo Player.ts
+import { Player } from "./models/Player.js";
+
 
 // Cria uma aplicação Express
 // A função express() devolve um objeto que representa o servidor da aplicação
 const app: Express = express();
+
+// Middleware para permitir que o servidor aceite requisições com corpo em formato JSON
+app.use(express.json());
+
+// Instanciação de um jogador ultilizando a classe Player
+// Criamos (instanciamos) um novo jogador chamado "Hero" com 100 de saúde e nível 1
+// Apartir da classe Playes que foi importada do arquivo Player.ts
+let player1: Player = new Player("Hero", 100, 5);
+
+// Rota GET para obter informações do jogador
+// Quando o cliente fizer uma requisição GET para a rota "/player", o servidor responderá com os dados do jogador
+// A função de callback recebe dois parâmetros: req (requisição) e res (resposta)
+app.get("/player", (req: Request, res: Response) => {
+  res.json({
+    message: "Informações do jogador",
+    player:player1,
+  }); 
+});
+//rota POST para atacar o jogador
+// Quando o cliente fizer uma requisição POST para a rota "/player/attack", o servidor chamará o método attack() do jogador
+// do jogador
+// É ultilizafa para enviar dados para o servidor, nesse caso, o ataque do jogador
+// como neste case, onde o jogador realiza uma ação (como acionar um comportamento de ataque)
+// que é o metodo attack() do Jogador.
+// A função de callback recebe dois parâmetros: req (requisição) e res (resposta)
+app.post("/player/attack", (req: Request, res: Response) => {
+  const attackMessage = player1.attack(); //chama o método attack() do jogador
+  // Retorna uma resposta JSON com a mensagem de ataque
+  // para o cliente que fez a requisição
+  res.json({
+    message: attackMessage,
+  });
+});
+
+// Rota para receber o dano 
+// Quando o usúario acessar a rota "/player/take-damage" com uma requisição POST, o servidor chamará o método takeDamage() do jogador
+// método takeDamage() do jogador, pasando o valor do dano recebido no corpo da requisição (req.body.amount).
+//parâmetro
+app.post("/player/take-damage", (req: Request, res: Response) => {
+  const { damage } = req.body;
+  const damageMessage = player1.takeDamage(damage);
+  // Retorna uma resposta JSON com a mensagem de dano
+  // para o cliente que fez a requisição
+  res.json({
+    // Retorna a mensagem do dano recebido
+    action: damageMessage,
+    // Retorna o valor da saúde atual do jogador
+    currentHealth: player1.health,
+    // Retorna o nível atual do jogador
+    currentLevel: player1.level,
+  });
+});
+
 
 // Define a porta onde o servidor ficará disponível
 // Neste caso, o servidor poderá ser acessado pela porta 8081
@@ -15,4 +71,8 @@ const PORT: number = 8081;
 // O método listen() faz o servidor começar a "escutar" requisições HTTP
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log("Rotas disponiveis:");
+  console.log(`GET http://localhost:${PORT}/player - Obter informações do jogador`);
+  console.log(`POST http://localhost:${PORT}/player/attack - Jogador realiza ataque`);
+  console.log(`POST http://localhost:${PORT}/player/take-damage - Jogador recebe dano`);
 });
---
+++ src/models/Player.ts
@@ -0,0 +1,43 @@
+// a palavra "classe" define que estamos criando um molde.
+// A palavra "export" permite que esse arquivo seja usado
+//  por outros arquivos (como o app.ts)
+export class Player {
+    public name: string; // O nome do jogador em (texto)
+    public health: number; // A saúde do jogador (número)
+    public level: number; // O nível do jogador (número)
+
+    // Contrutores (O contutor é um metodo especial e executado
+    //  automaticamente quando a classe é instanciada apenas uma unica vez)
+    constructor(name: string, health: number = 100, level: number = 1) {
+        // A palavra "this" faz referência a própria classe, ou seja:
+        // "pegue o atributo 'name' da classe Player e atribua o valor
+        //  do parametro 'name' a ele"
+        this.name = name; 
+        this.health = health; 
+        this.level = level; 
+    }   
+
+    // Métodos (comportamento da classe)
+    // Métodos são as funções que a classe pode executar, ou seja, são os
+    //  comportamentos da classe.
+    //O Método "attack" é um método que retorna uma string.
+    public attack(): string {
+        const damage: number = this.level * 10; // Calcula o dano com base no nível do jogador
+        return `${this.name} atacou e causou ${damage} de dano!`; 
+
+}
+
+// O método de takeDamage é um método que recebe um parâmetro de dano (amount) e não retorna nada (void).
+// não retorna nada (void).
+    public takeDamage(amount: number): string {
+        this.health -= amount; // Reduz a saúde do jogador com base no dano recebido
+        if (this.health < 0) {
+            this.health = 0; // Garante que a saúde não fique negativa
+            return `${this.name} foi derrotado!`; // Retorna uma mensagem indicando que o jogador foi derrotado
+        }
+        
+    
+    return `${this.name} recebeu ${amount} de dano e agora tem ${this.health} de saúde.`;
+    }
+
+}
